bf772e40f92657a79c2123d4a146e8da8fb041b6
Tom Dawes <tomdawes91@gmail.com>
2019-09-03 17:23:38 +0100

    In fact just remove the merge function

--- packages/babel-plugin/src/index.ts
+++ packages/babel-plugin/src/index.ts
@@ -1,21 +1,8 @@
 import * as Babel from "@babel/core";
-import { VisitNodeFunction } from "@babel/traverse";
 import * as _ from "lodash";
 import visitComponentAndActions from "./components-and-actions";
 
-const mergeVisitors = <S>(...visitors: Array<Babel.Visitor<S>>) =>
-  _.mergeWith(
-    ...visitors,
-    <P>(a: VisitNodeFunction<S, P>, b: VisitNodeFunction<S, P>) => (
-      path: Babel.NodePath<P>,
-      state: S,
-    ) => {
-      a.call(state, path, state);
-      b.call(state, path, state);
-    },
-  );
-
 export default (babel: typeof Babel): Babel.PluginObj => ({
   name: "prodo",
-  visitor: mergeVisitors(visitComponentAndActions(babel)),
+  visitor: visitComponentAndActions(babel),
 });
