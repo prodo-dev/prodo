950cd9ff431419970be3ab62451f82d0bc51d00e
Tom Dawes <tomdawes91@gmail.com>
2019-09-03 17:34:02 +0100

    Better shallow comparison function

--- packages/core/src/connect.tsx
+++ packages/core/src/connect.tsx
@@ -26,20 +26,37 @@ const valueExtractor = (store: any, watched: any, prefixPath: any = []) => (
 };
 
 const shallowEqual = (objA: any, objB: any): boolean => {
-  if (objA === objB) {
+  if (Object.is(objA, objB)) {
     return true;
   }
+
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+
   const keysA = Object.keys(objA);
   const keysB = Object.keys(objB);
+
   if (keysA.length !== keysB.length) {
     return false;
   }
 
-  for (const key of keysA) {
-    if (!objB.hasOwnProperty(key) || objA[key] !== objB[key]) {
+  // Test for A's keys different from B.
+  // tslint:disable-next-line:prefer-for-of
+  for (let i = 0; i < keysA.length; i++) {
+    if (
+      !Object.prototype.hasOwnProperty.call(objB, keysA[i]) ||
+      !Object.is(objA[keysA[i]], objB[keysA[i]])
+    ) {
       return false;
     }
   }
+
   return true;
 };
 
