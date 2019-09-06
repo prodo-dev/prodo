2927f8b0903de2720e5f50b30e27d0c57d036b0c
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2019-09-06 11:55:06 +0100

    Relax streams test requirement

--- packages/stream-plugin/tests/index.test.ts
+++ packages/stream-plugin/tests/index.test.ts
@@ -32,7 +32,7 @@ describe("stream plugin", () => {
     });
 
     await store.dispatch(startCounter)(10);
-    await new Promise(res => setTimeout(() => res(), 10));
+    await new Promise(res => setTimeout(() => res(), 30));
     expect(store.universe.streams.count).toBeLessThan(5);
     await new Promise(res => setTimeout(() => res(), 100));
     expect(store.universe.streams.count).toBeGreaterThan(5);
