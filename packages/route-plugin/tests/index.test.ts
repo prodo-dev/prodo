d6ca986099090dee3976414f80de3a5d712a014c
Tom Dawes <tomdawes91@gmail.com>
2019-09-10 17:29:47 +0100

    Add test

---
+++ packages/route-plugin/tests/index.test.ts
@@ -0,0 +1,39 @@
+import { createModel } from "@prodo/core";
+import { createMemoryHistory } from "history";
+import routePlugin, { push, replace } from "../src";
+
+const model = createModel<{}>().with(routePlugin);
+
+describe("random plugin", () => {
+  it("", async () => {
+    const history = createMemoryHistory();
+
+    const { store } = model.createStore({
+      initState: {},
+      route: { history },
+    });
+
+    let route = store.universe.route;
+    expect(route.path).toBe("/");
+    expect(route.params).toEqual({});
+    expect(history.length).toBe(1);
+
+    route = (await store.dispatch(push)("/test-1")).route;
+    expect(route.path).toBe("/test-1");
+    expect(route.params).toEqual({});
+    expect(history.length).toBe(2);
+
+    route = (await store.dispatch(push)({
+      path: "/test-2",
+      params: { foo: "bar" },
+    })).route;
+    expect(route.path).toBe("/test-2");
+    expect(route.params).toEqual({ foo: "bar" });
+    expect(history.length).toBe(3);
+
+    route = (await store.dispatch(replace)("/test-3")).route;
+    expect(route.path).toBe("/test-3");
+    expect(route.params).toEqual({});
+    expect(history.length).toBe(3);
+  });
+});
