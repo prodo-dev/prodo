03d878d868ad53d4e6c04d277a00eb64b40b8cf7
Bruno Marnette <marnette.bruno@gmail.com>
2019-09-11 16:07:56 +0100

    make tslint happy

--- packages/logger-plugin/src/index.tsx
+++ packages/logger-plugin/src/index.tsx
@@ -1,17 +1,21 @@
 import { ProdoPlugin } from "@prodo/core";
 
-export type LoggerConfig = {
+export interface LoggerConfig {
   logger?: boolean;
-};
+}
 
 const init = (config: LoggerConfig) => {
-  if (config.logger) console.log("@prodo/logger is on");
+  if (config.logger) {
+    // tslint:disable-next-line
+    console.log("@prodo/logger is on");
+  }
 };
 
 const loggerPlugin: ProdoPlugin<LoggerConfig, {}, {}, {}> = {
   name: "logger",
   init,
   onCompletedEvent: e => {
+    // tslint:disable-next-line
     console.log(e);
   },
 };
