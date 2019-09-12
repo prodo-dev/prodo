fa283f68eb44b54f9fe1193afa6eb3e6d50962ca
Bruno Marnette <marnette.bruno@gmail.com>
2019-09-12 14:14:15 +0100

    fix babel config for test

--- examples/kanban/src/main.tsx
+++ examples/kanban/src/main.tsx
@@ -1,22 +1,48 @@
-import { ProdoProvider } from "@prodo/core";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "react-error-boundary";
 import App from "./components/App";
-import store from "./store";
 import "./styles.scss";
+import { model } from "./model";
 
-//@ts-ignore
-window.store = store; // for debugging
+const { Provider } = model.createStore({
+  logger: true,
+  initState: {
+    user: {
+      _id: "ted",
+      name: "ted",
+      imageUrl: "https://profiles.utdallas.edu/img/default.png",
+    },
+    currentBoardId: "B1",
+    boardsById: {
+      B1: {
+        _id: "B1",
+        title: "todos",
+        color: "blue",
+        lists: ["L1", "L2"],
+        users: ["ted"],
+      },
+    },
+    listsById: {
+      L1: { _id: "L1", title: "doing", cards: ["C1", "C2"] },
+      L2: { _id: "L2", title: "done-ish", cards: ["C3"] },
+    },
+    cardsById: {
+      C1: { _id: "C1", text: "Hello", date: new Date(), color: "white" },
+      C2: { _id: "C2", text: "World", date: new Date(), color: "green" },
+      C3: { _id: "C3", text: "everything", date: new Date(), color: "blue" },
+    },
+  },
+});
 
 ReactDOM.render(
   <ErrorBoundary>
     <BrowserRouter>
-      <ProdoProvider value={store}>
+      <Provider>
         <App />
-      </ProdoProvider>
+      </Provider>
     </BrowserRouter>
   </ErrorBoundary>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
--- examples/kanban/tests/render.test.tsx
+++ examples/kanban/tests/render.test.tsx
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import * as React from "react";
 import App from "../src/components/App";
 import { model } from "../src/model";
@@ -9,9 +10,11 @@ describe("components", () => {
   it("can render with initial store", async () => {
     const { Provider } = model.createStore({ initState });
     render(
-      <Provider>
-        <App />
-      </Provider>,
+      <BrowserRouter>
+        <Provider>
+          <App />
+        </Provider>
+      </BrowserRouter>,
     );
   });
 });
