e3d4e62580b746828ae397cfe43b6e3878194add
Andreja <andreja.kogovsek1@gmail.com>
2019-09-13 11:08:32 +0100

    Add the devtools plugin to pass messages

---
+++ packages/devtools-plugin/src/index.tsx
@@ -0,0 +1,33 @@
+import { ProdoPlugin } from "@prodo/core";
+
+export interface Message {
+  destination: "devtools" | "app";
+  type: string;
+  contents: any;
+}
+
+export interface DevToolsConfig {
+  logger?: boolean;
+}
+
+const init = (config: DevToolsConfig) => {
+  if (config.logger) {
+    // tslint:disable-next-line
+    console.log("@prodo/devtools is on");
+  }
+};
+
+const devtoolsPlugin: ProdoPlugin<DevToolsConfig, {}, {}, {}> = {
+  name: "devtools",
+  init,
+  onCompletedEvent: e => {
+    const message: Message = {
+      destination: "devtools",
+      type: "completedEvent",
+      contents: e,
+    };
+    window.postMessage(message, "*");
+  },
+};
+
+export default devtoolsPlugin;
