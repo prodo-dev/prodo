249194c52b50da33f89d005a08e31c8606139c00
Bruno Marnette <marnette.bruno@gmail.com>
2019-09-14 21:38:51 +0100

    removing example/kanban as it was a wrong version...

--- examples/kanban/kanban/src/model.tsx
+++
@@ -1,8 +0,0 @@
-import { createModel } from "@prodo/core";
-import effect from "@prodo/effect";
-import logger from "@prodo/logger";
-import { State } from "./types";
-export const model = createModel<State>()
-  .with(effect)
-  .with(logger);
-export const { state, watch, dispatch } = model.ctx;
--- examples/kanban/src/components/utils.tsx
+++
@@ -1,5 +0,0 @@
-export const findCheckboxes = (text: string) => {
-  const checkboxes = text.match(/\[(\s|x)\]/g) || [];
-  const checked = checkboxes.filter(checkbox => checkbox === "[x]").length;
-  return { total: checkboxes.length, checked };
-};
