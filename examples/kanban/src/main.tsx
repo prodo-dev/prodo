68d6cd293ea6606a01980375dd4be6764306983d
Bruno Marnette <marnette.bruno@gmail.com>
2019-09-14 21:44:45 +0100

    fix type error on master

--- examples/kanban/src/main.tsx
+++ examples/kanban/src/main.tsx
@@ -1,10 +1,9 @@
-import { ProdoProvider } from "@prodo/core";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import ErrorBoundary from "react-error-boundary";
 import App from "./components/App";
-import store from "./store";
+import { store, Provider } from "./store";
 import "./styles.scss";
 
 //@ts-ignore
@@ -13,10 +12,10 @@ window.store = store; // for debugging
 ReactDOM.render(
   <ErrorBoundary>
     <BrowserRouter>
-      <ProdoProvider value={store}>
+      <Provider>
         <App />
-      </ProdoProvider>
+      </Provider>
     </BrowserRouter>
   </ErrorBoundary>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
