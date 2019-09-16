8de9c4b6e7d470608f059ae25b0109aedcd08309
Jake Runzer <jakerunzer@gmail.com>
2019-09-16 13:19:31 +0100

    use logger plugin in todo app

--- examples/todo-app/src/model.ts
+++ examples/todo-app/src/model.ts
@@ -1,10 +1,13 @@
 import { createModel } from "@prodo/core";
 import effectPlugin from "@prodo/effect";
+import loggerPlugin from "@prodo/logger";
 
 export interface State {
   todos: { [key: string]: { text: string; done: boolean } };
 }
 
-export const model = createModel<State>().with(effectPlugin);
+export const model = createModel<State>()
+  .with(effectPlugin)
+  .with(loggerPlugin);
 
 export const { state, watch, effect, dispatch } = model.ctx;
