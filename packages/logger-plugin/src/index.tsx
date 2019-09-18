07be9a61bc65f40188b4b2e1155ec0b569b972d0
Bruno Marnette <marnette.bruno@gmail.com>
2019-09-18 20:32:14 +0100

    adding some colors and grouping to the logger

--- packages/logger-plugin/src/index.tsx
+++ packages/logger-plugin/src/index.tsx
@@ -16,10 +16,33 @@ const loggerPlugin: ProdoPlugin<LoggerConfig, {}, {}, {}> = {
   init,
   onCompletedEvent: (e, config) => {
     if (config.logger) {
-      // tslint:disable-next-line:no-console
-      console.log(e);
+      prettyPrint(e);
     }
   },
 };
+
+function prettyPrint(e: any) {
+  // tslint:disable:no-console
+  console.groupCollapsed(
+    "%c%s",
+    "background: #20e3a0; font-size: 12px; font-weight: bold; padding: 4px; border-radius: 3px;",
+    e.actionName,
+  );
+  console.log("%c%s", "background: lightgrey; padding: 3px;", "origin", {
+    id: e.id,
+    parent: e.parentId,
+    args: e.args,
+  });
+  console.log("%c%s", "background: lightblue; padding: 3px;", "data", {
+    prev: e.prevUniverse,
+    next: e.nextUniverse,
+    patches: e.patches,
+  });
+  console.log("%c%s", "background: #f7b1b1; padding: 3px;", "effects", {
+    records: e.recordedEffects,
+    rerender: e.rerender,
+  });
+  console.groupEnd();
+}
 
 export default loggerPlugin;
