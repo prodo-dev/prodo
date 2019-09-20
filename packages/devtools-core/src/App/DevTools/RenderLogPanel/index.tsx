90f2fa73e7f87696eb4db92e3efdb19293371d4e
Andreja <andreja.kogovsek1@gmail.com>
2019-09-20 14:50:52 +0100

    Quick re-do of the render panel

--- packages/devtools-core/src/App/DevTools/RenderLogPanel/index.tsx
+++ packages/devtools-core/src/App/DevTools/RenderLogPanel/index.tsx
@@ -1,20 +1,40 @@
 import * as _ from "lodash";
 import * as React from "react";
 import { state, watch } from "../../../model";
-import { Render } from "../../../types";
 import { RenderLogRow } from "./RenderLogRow";
 
 export const emptyRenderLogMessage = "Render log is empty.";
 
 export const RenderLogPanel = () => {
   const renderLog = watch(state.app.renderLog);
+  console.log(renderLog);
+
+  const components = {};
+  renderLog.map(entry => {
+    if (!components[entry.componentId]) {
+      components[entry.componentId] = { total: 1 };
+      components[entry.componentId][entry.actionName] = 1;
+    } else {
+      components[entry.componentId].total += 1;
+      components[entry.componentId][entry.actionName] = components[
+        entry.componentId
+      ][entry.actionName]
+        ? components[entry.componentId][entry.actionName] + 1
+        : 1;
+    }
+  });
+  console.log(components);
 
   return (
     <div className="renderLogPanel" data-testid="renderLogPanel">
       {renderLog.length === 0
         ? emptyRenderLogMessage
-        : renderLog.map((render: Render, index: number) => (
-            <RenderLogRow render={render} key={index} />
+        : Object.keys(components).map((componentId: string, index: number) => (
+            <RenderLogRow
+              componentId={componentId}
+              renders={components[componentId]}
+              key={index}
+            />
           ))}
     </div>
   );
