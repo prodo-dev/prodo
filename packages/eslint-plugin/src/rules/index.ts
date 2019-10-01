c8dddbb267ed9f5c80d4dc21f38b16b2f90b71c5
Konstantinos Primerakis <konstantinos@prodo.ai>
2019-10-01 11:56:47 +0100

    index fix

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -1,9 +1,9 @@
-import noActionReturn from "./actions";
+import checkActions from "./actions";
 import noStateAccess from "./no-state-access";
 import requireDispatchedActionIsCalled from "./require-dispatched-action-is-called";
 
 export default {
-  "no-action-return": noActionReturn,
+  "check-actions": checkActions,
   "no-state-access": noStateAccess,
   "require-dispatched-action-is-called": requireDispatchedActionIsCalled,
 };
