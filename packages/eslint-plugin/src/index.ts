1dea74290b42ed18f97add20ae1f4faf572df79d
Konstantinos Primerakis <konstantinos@prodo.ai>
2019-10-02 13:50:01 +0100

    cleaner export

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,6 +1,5 @@
 import recommended from "./configs/recommended";
 import rules from "./rules";
 
-export { rules };
 const configs = { recommended };
-export { configs };
+export { rules, configs };
