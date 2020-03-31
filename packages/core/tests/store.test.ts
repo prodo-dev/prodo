c94c2db5cc4e697257b5626c0ccf60c894e46242
Tom Dawes <tomdawes91@gmail.com>
2020-03-31 21:03:22 +0100

    Fix multiple dispatches

--- packages/core/tests/store.test.ts
+++ packages/core/tests/store.test.ts
@@ -2,10 +2,12 @@ import { createModel } from "../src";
 
 interface State {
   count: number;
+  counts: { [key: string]: number };
 }
 
 const initState: State = {
   count: 0,
+  counts: {},
 };
 
 const model = createModel<State>();
@@ -18,5 +20,23 @@ describe("store", () => {
     expect(Object.keys(store.history)).toHaveLength(0);
     expect(store.watchTree.subs.size).toBe(0);
     expect(store.watchTree.esubs.size).toBe(0);
+  });
+
+  it("can dispatch and await multiple actions", async () => {
+    const { store } = model.createStore({ initState });
+
+    const action = model.action(({ state }) => async (key: string) => {
+      await new Promise(r => setTimeout(r, 100));
+      state.counts[key] = 1;
+    });
+
+    const results = await Promise.all([
+      store.dispatch(action)("foo"),
+      store.dispatch(action)("bar"),
+    ]);
+
+    results.forEach(({ state }) => {
+      expect(state.counts).toEqual({ foo: 1, bar: 1 });
+    });
   });
 });
